bbf843699cec7e2c62cf09fa54650b6efc91439f
91897 <91897@192.168.0.3>
2020-03-08 23:33:58 +0530

    one

---
+++ Calculator/src/main/java/App/Driver.java
@@ -0,0 +1,18 @@
+package App;
+
+import Math.Divide;
+import Math.Multiply;
+import Math.Subtract;
+import Math.Add;
+
+public class Driver {
+public static void main(String [] args) { 
+	
+System.out.println(Multiply.compute(1, 2));
+System.out.println(Divide.compute(10.0, 5.0));
+System.out.println(Add.compute(1,2));
+System.out.println(Subtract.compute(3,2));
+
+}
+
+}
---
+++ Calculator/src/test/java/DivideTest.java
@@ -0,0 +1,17 @@
+//package Test;
+
+
+
+import org.junit.Assert;
+
+import Math.Divide;
+import org.junit.Test;
+
+public class DivideTest {
+
+	//@Test
+	public void testDivide() {
+		Assert.assertEquals(69,Divide.compute(7,1));
+	}
+
+}
